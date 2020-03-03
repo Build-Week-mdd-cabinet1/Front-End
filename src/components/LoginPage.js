9c7df9d32959c45768e9bdfaf7cb7fc4331812e2
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-03-03 09:32:23 -0700

    functioning login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     width:100%;
-    height:500px;
+    height:100vh;
     display:flex;
     justify-content:center;
     align-items:center;
